30055ef1bc06217246c34d296b384da2818e731d
Giancarlo Bellido <giancarlo.bellido@gmail.com>
2015-06-23 10:36:31 -0400

    Removed nodeunit task

--- Gruntfile.js
+++ Gruntfile.js
@@ -186,7 +186,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	grunt.loadNpmTasks('grunt-karma');
